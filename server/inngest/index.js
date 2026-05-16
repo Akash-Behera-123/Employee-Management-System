ed795d4afea9ce520c9581e87d2331d4f7507df3
Akash-Behera-cmd <akashbehera955649@gmail.com>
2026-05-16 17:17:21 +0530

    Backend Completed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -164,7 +164,7 @@ const leaveApplicationReminder = inngest.createFunction(
 const attendanceReminderCron = inngest.createFunction(
   {
     id: "attendance-reminder-cron",
-    triggers: [{ cron: "0 0 6 * * *" }],
+    triggers: [{ cron: "TZ=Asia/Kolkata 30 11 * * *" }],
   },
 
   async ({ step }) => {
